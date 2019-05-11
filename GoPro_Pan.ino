0fd84d3280677966bd2217261007a5e9b64adda2
xj4v1x <javi.lazcano@gmail.com>
2019-05-11 23:46:05 +0200

    Add files via upload

---
+++ GoPro_Pan.ino
@@ -0,0 +1,120 @@
+#include <Servo.h>
+ 
+Servo myservo;  // crea el objeto servo
+ 
+int pos = 0;    // posicion del servo
+int BotonA = 5;
+int BotonB = 6;
+bool Derecha = false;
+int opt = 0;
+int minutos = 0;
+int segundos = 0;
+long tiempo = 40000;
+int servo = 7;
+int buzz = 8;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode (BotonA, INPUT);
+  pinMode (BotonB, INPUT);
+  pinMode(servo, OUTPUT);
+  pinMode(buzz, OUTPUT);
+  myservo.attach(servo);  // vincula el servo al pin digital 7
+  myservo.write(90);              
+  delay (500);
+  myservo.detach();
+  Sentido();
+  Tiempo();
+  Gira();
+  Fin();
+}
+ 
+void loop() {
+ 
+}
+
+void Sentido(){
+  opt = 0;
+ 
+  while (opt==0) {
+    if (digitalRead(BotonA)==HIGH){     
+      delay(300);
+      beep(277,200);
+      opt = 1;
+      Derecha = true;
+      myservo.attach(servo);
+      myservo.write(0);
+      delay(500);
+      myservo.detach();
+    }
+    if (digitalRead(BotonB)==HIGH){
+      delay(300);
+      beep(277,200);
+      opt = 2;
+      Derecha = false;
+      myservo.attach(servo);
+      myservo.write(180);
+      delay(500);
+      myservo.detach();
+    }
+  }
+}
+
+void Tiempo(){
+  opt = 0;
+  while (opt==0) {
+    if (digitalRead(BotonA)==HIGH){     
+      delay(300);
+      beep(277,200);
+      tiempo = tiempo/2;
+      if (tiempo<500) {
+        tiempo=20000;
+        beep(440 ,400);
+      }
+      Serial.println(((tiempo/1000)*180)/60);
+    }
+    if (digitalRead(BotonB)==HIGH){
+      delay(300);
+      beep(440 ,400);
+      opt = 1;
+    }
+  }
+}
+
+void Gira(){
+  myservo.attach(servo);
+  Serial.print(Derecha);
+  if (Derecha == true) {
+    myservo.write(0);
+    delay(500);
+    for (int i=0; i <= 180; i++) {
+      myservo.write(i);        
+      delay (tiempo);       
+      Serial.println(i);
+    } 
+  } else {
+    myservo.write(180);
+    delay(500);
+    for (int i=180; i >= 0; i--) {
+      myservo.write(i);              
+      delay (tiempo);
+      Serial.println(i);
+    } 
+  }
+   myservo.detach();
+}
+
+void Fin() {
+  beep(261,500);
+  beep(277,500);
+  beep(294,500);
+  
+  
+
+  
+}
+void beep(int tono, int espera) {
+  tone(8, tono);
+  delay(espera);                 // Espera
+  noTone(8);
+}
